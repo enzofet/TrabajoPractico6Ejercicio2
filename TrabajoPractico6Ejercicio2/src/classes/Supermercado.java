3ab04ecc0f976b4be7011fcebf74b6a41677bbac
Enzo Fet <enzofet1998@gmail.com>
2025-09-08 17:22:41 -0300

    Creación metodos supermercado

--- TrabajoPractico6Ejercicio2/src/classes/Supermercado.java
+++ TrabajoPractico6Ejercicio2/src/classes/Supermercado.java
@@ -14,41 +14,65 @@ import java.util.TreeSet;
  * @author Enzo_2
  */
 public class Supermercado {
+
     private TreeSet<Producto> catalogo;
-    
-    public Supermercado(){}
-    
-    public void anadirProducto(Producto p){
+
+    public Supermercado() {
+    }
+
+    public void anadirProducto(Producto p) {
         catalogo.add(p);
     }
-    
-    public void sobreescribirProducto(Producto p){
-        if(catalogo.contains(p)){
-            
-        }
-    }
-    
-    
-    public void eliminarProducto(int codigo){
-        for(Producto p : catalogo){
-            if(codigo == p.getCodigo()){
-                catalogo.remove(p);
+
+    public void sobreescribirProducto(Producto p) {
+        for (Producto producto : catalogo) {
+            if (producto.equals(p.getCodigo())) {
+                catalogo.remove(producto);
+                catalogo.add(p);
             }
         }
+
     }
     
-    public List buscarProductos(String rubro){
+    public List buscarPorPrecio(double precioMin, double precioMax){
         List<Producto> productos = new ArrayList();
         for(Producto p : catalogo){
-            if(p.getRubro().equalsIgnoreCase(rubro)){
+            if(p.getPrecio() > precioMin && p.getCodigo() < precioMax){
                 productos.add(p);
             }
         }
         return productos;
     }
     
-    
-    
+    public List buscarPorNombre(String nombre){
+        List<Producto> productos = new ArrayList();
+        for(Producto p : catalogo){
+            if(p.getNombre().equalsIgnoreCase(nombre)){
+                productos.add(p);
+            }
+        }
+        return productos;
+        
+    }
+
+    public void eliminarProducto(int codigo) {
+        for (Producto p : catalogo) {
+            if (codigo == p.getCodigo()) {
+                catalogo.remove(p);
+            }
+        }
+    }
+
+    public List buscarProductosRubro(Rubro rubro) {
+        List<Producto> productos = new ArrayList();
+        for (Producto p : catalogo) {
+            if (p.getRubro() == rubro) {
+                productos.add(p);
+            }
+        }
+        return productos;
+    }
+
     public TreeSet<Producto> getCatalogo() {
         return catalogo;
     }
@@ -56,6 +80,5 @@ public class Supermercado {
     public void setCatalogo(TreeSet<Producto> catalogo) {
         this.catalogo = catalogo;
     }
-    
-    
+
 }
